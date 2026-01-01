a7d21aa0624060d10bf78480ed7aebd5197b27c1
Abdirahman Cabdi <speedextra008@gmail.com>
2026-01-01 18:22:47 +0300

    Add TODOs for function and button click handling
    Added TODO comments for function creation and button interaction.

---
+++ script.js
@@ -0,0 +1,10 @@
+// TODO 10: Create a function
+
+// TODO 11: Make the function run when the button is clicked
+
+/*
+Hints:
+- Functions have names
+- Buttons can listen for clicks
+- Browsers can show messages
+*/
